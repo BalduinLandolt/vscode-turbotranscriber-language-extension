c4ffcf770d92aa588e05c4757bccfb7887f9ec5d
BalduinLandolt <balduin.landolt@unibas.ch>
2020-07-18 14:21:27 +0200

    Overhaul: Replace nodejs server with java server based on original TurboTranscriber

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -13,50 +13,95 @@ import {
 	TransportKind
 } from 'vscode-languageclient';
 
+import { existsSync } from "fs";
+
 let client: LanguageClient;
+
+// Server Main Class name
+const main: string = 'TTRLSPLauncher';
+
+// Server Launcher
+// TODO: get this from settings
+const serverLauncher: string = 'TurboTranscriber-LSP-Server-0.0.5-jar-with-dependencies.jar'
+// const serverLauncher: string = 'TurboTranscriber-LSP-Server-0.0.5-jar-with-dependencies'
 
 export function activate(context: ExtensionContext) {
 	console.log('Client activating...');
+	
 
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	// Java Runtime
+	const { JAVA_HOME } = process.env;
+	console.log(`Using java from JAVA_HOME: ${JAVA_HOME}`);
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
+	// If java home is available continue.
+	if (JAVA_HOME) {
+		// Java execution path.
+		let executable: string = path.join(JAVA_HOME, 'bin', 'java');
+		console.log(executable);
+		
 
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'ttr' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
+		// Path to launcher
+		// TODO: ensure this is dynamic
+		let launcherPath: string = path.join(__dirname, '..', '..', 'server', serverLauncher);
+		console.log(launcherPath);
+		
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'ttr-lsp-server',
-		'TurboTranscriber Language Server',
-		serverOptions,
-		clientOptions
-	);
+		// TODO: do I need this?
+		const args: string[] = ['-jar', launcherPath];
 
-	// Start the client. This will also launch the server
-	client.start();
+		// Set the server options 
+		// -- java execution path
+		// -- argument to be pass when executing the java command
+		let serverOptions: ServerOptions = {
+			command: executable,
+			args: [...args],
+			// args: [...args, main],
+			// args: [main],
+			options: {}
+		};
+
+		// Options to control the language client
+		let clientOptions: LanguageClientOptions = {
+			// Register the server for plain text documents
+			documentSelector: [{ scheme: 'file', language: 'ttr' }],
+			synchronize: {
+				// Notify the server about file changes to '.clientrc files contained in the workspace
+				fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+			}
+		};
+
+		// Create the language client and start the client.
+		client = new LanguageClient(
+			'ttr-lsp-server',
+			'TurboTranscriber Language Server',
+			serverOptions,
+			clientOptions
+		);
+
+		// Start the client. This will also launch the server
+		client.start();
+	}
+
+	// ----------------------------------------------------
+
+	// // The server is implemented in node
+	// let serverModule = context.asAbsolutePath(
+	// 	path.join('server', 'out', 'server.js')
+	// );
+	// // The debug options for the server
+	// // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	// let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+	// // If the extension is launched in debug mode then the debug server options are used
+	// // Otherwise the run options are used
+	// let serverOptions: ServerOptions = {
+	// 	run: { module: serverModule, transport: TransportKind.ipc },
+	// 	debug: {
+	// 		module: serverModule,
+	// 		transport: TransportKind.ipc,
+	// 		options: debugOptions
+	// 	}
+	// };
 }
 
 export function deactivate(): Thenable<void> | undefined {
